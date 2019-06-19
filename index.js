0c84604f7841b5989453272422c1e3abac40bdc9
ekater1na <ekaterina_l@tut.by>
2019-06-19 18:56:40 +0300

    feat: add solution

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-// The scope of `random` is too loose 
-const getRandEvent = () => {
-  const random = Math.floor(Math.random() * 3);
-  if (random === 0) {
-    return 'Marathon';
-  } else if (random === 1) {
-    return 'Triathlon';
-  } else if (random === 2) {
-    return 'Pentathlon';
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+let storyWords = story.split(' ');
+
+let betterWords = storyWords.filter(function(word) {
+                  return !unnecessaryWords.includes(word)
+                  })
+
+let reallyCount = 0;
+let veryCount = 0;
+let basicallyCount = 0;
+
+storyWords.forEach(word => {
+  if (word == 'really') {
+  reallyCount += 1}
+ else if 
+  (word == 'very') {
+   veryCount += 1}
+  else if 
+  (word == 'basically') {
+   basicallyCount += 1}
+});
+
+let sentenceCount = 0;
+
+storyWords.forEach(word => {
+  if (word[word.length-1] === '.' || word[word.length-1] === '!') {
+    sentenceCount += 1;
   }
-};
+});
 
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-let days;
-  if (event === 'Marathon') {
-     days = 50;
-  } else if (event === 'Triathlon') {
-     days = 100;
-  } else if (event === 'Pentathlon') {
-     days = 200;
+const paragraphStats = () => {
+  console.log('Your paragraph has ' + sentenceCount + ' sentences');
+ console.log('All of these sentences consist of ' + betterWords.length + ' words');
+ console.log('You used really ' + reallyCount + ' times');
+ console.log('You used very ' + veryCount + ' times');
+ console.log('You used basically ' + basicallyCount + ' times');
   }
 
-  return days;
-};
+paragraphStats();
 
-// The scope of `name` is too tight 
-const name = 'Nala';
-const logEvent = (event, name) => {
-  console.log(`${name}'s event is: ${event}`);
-};
-
-const logTime = (days, name) => {
-  
-  console.log(`${name}'s time to train is: ${days} days`);
-};
-
-const event = getRandEvent();
-const days = getTrainingDays(event);
-
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-logEvent(event, name);
-logTime(days, name);
-
-const event2 = getRandEvent();
-const days2 = getTrainingDays(event2);
-const name2 = 'Warren';
-
-logEvent(event2, name2);
-logTime(days2, name2);
+betterWords = betterWords.join(' ')
+console.log(betterWords)
