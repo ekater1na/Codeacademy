b472bffdde03af30061b6f4c37ac8246c875c5ba
ekater1na <ekaterina_l@tut.by>
2019-07-07 19:40:31 +0300

    feat: add solution

--- index.js
+++ index.js
@@ -1,79 +1,72 @@
-class Media {
-  constructor(title) {
-    this._title = title;
-    this._isCheckedOut = false;
-    this._ratings = [];
-  }
-  get title() {
-    return this._title;
-  }
-  get isCheckedOut() {
-    return this._isCheckedOut;
-  }
-  get ratings() {
-    return this._rating;
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name, 
+    this._level = level,
+    this._numberOfStudents = numberOfStudents;
   }
   
-  set isCheckedOut(checkIt) {
-    this._isCheckedOut = checkIt
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
   }
   
-  toggleCheckOutStatus() {
-    this._isCheckedOut = !this._isCheckedOut
-  }
-    
-  getAverageRating() {
-   let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
-    let lengthOfArray =  this._ratings.length;
-    let averageValue = ratingsSum / lengthOfArray;
-    return averageValue;
+  set numberOfStudents(numOfStud) {
+    if (typeof numOfStud === 'number') {
+      this._numberOfStudents = numOfStud;
+    } else {
+      console.log('Invalid input: numberOfStudents must be set to a Number.')
+    }
   }
   
- addRating(rating) {
-    this._ratings.push(rating);
+  quickFacts() {
+    console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
   }
-}
-
-class Book extends Media {
-  constructor(author, title, pages) {
-    super(title);
-    this._author = author;
-    this._pages = pages;
-  }
-  get author() {
-    return this.author;
-  }
-  get pages() {
-    return this.pages;
+  
+  static pickSubstituteTeacher(substituteTeachers) {
+    let index = Math.floor((substituteTeachers.length-1) * Math.random());
+    return substituteTeachers[index];
   }
 };
 
-class Movie extends Media {
-  constructor(director, title, runTime) {
-    super(title);
-    this._director = director;
-    this._runTime = runTime;
+class PrimarySchool extends School{
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, 'primary', numberOfStudents);
+   
+    this._pickupPolicy = pickupPolicy;
   }
-  get director() {
-    return this.director;
-  }
-  get runTime() {
-    return this.runTime;
+  get pickupPolicy() {
+    return this._pickupPolicy;
   }
 };
 
-let historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
-historyOfEverything.toggleCheckOutStatus();
-historyOfEverything.addRating(4);
-historyOfEverything.addRating(5);
-historyOfEverything.addRating(5);
-historyOfEverything.getAverageRating();
-console.log(historyOfEverything.getAverageRating());
+class MiddleSchool extends School{
+  constructor(name, numberOfStudents) {
+    super(name, 'middle', numberOfStudents);
+      }
+  };
 
-let speed = new Movie('Jan de Bont', 'Speed', 116);
-speed.toggleCheckOutStatus();
-speed.addRating(1);
-speed.addRating(1);
-speed.addRating(5);
-speed.getAverageRating();
-console.log(speed.getAverageRating());
+class HighSchool extends School{
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+};
+
+let lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+console.log(lorraineHansbury)
+lorraineHansbury.quickFacts();
+
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+let alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith)
+alSmith.quickFacts();
