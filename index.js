995ddc4c16abb0d60340a5d6ed6bf70cac3e2cbc
ekater1na <ekaterina_l@tut.by>
2019-07-07 17:28:16 +0300

    feat: add solution

--- index.js
+++ index.js
@@ -1,50 +1,79 @@
-// The scope of `random` is too loose 
-const getRandEvent = () => {
-  const random = Math.floor(Math.random() * 3);
-  if (random === 0) {
-    return 'Marathon';
-  } else if (random === 1) {
-    return 'Triathlon';
-  } else if (random === 2) {
-    return 'Pentathlon';
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
   }
-};
-
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-let days;
-  if (event === 'Marathon') {
-     days = 50;
-  } else if (event === 'Triathlon') {
-     days = 100;
-  } else if (event === 'Pentathlon') {
-     days = 200;
+  get title() {
+    return this._title;
   }
-
-  return days;
-};
-
-// The scope of `name` is too tight 
-const name = 'Nala';
-const logEvent = (event, name) => {
-  console.log(`${name}'s event is: ${event}`);
-};
-
-const logTime = (days, name) => {
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._rating;
+  }
   
-  console.log(`${name}'s time to train is: ${days} days`);
+  set isCheckedOut(checkIt) {
+    this._isCheckedOut = checkIt
+  }
+  
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut
+  }
+    
+  getAverageRating() {
+   let ratingsSum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+    let lengthOfArray =  this._ratings.length;
+    let averageValue = ratingsSum / lengthOfArray;
+    return averageValue;
+  }
+  
+ addRating(rating) {
+    this._ratings.push(rating);
+  }
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this.author;
+  }
+  get pages() {
+    return this.pages;
+  }
 };
 
-const event = getRandEvent();
-const days = getTrainingDays(event);
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+  get director() {
+    return this.director;
+  }
+  get runTime() {
+    return this.runTime;
+  }
+};
 
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-logEvent(event, name);
-logTime(days, name);
+let historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544)
+historyOfEverything.toggleCheckOutStatus();
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+historyOfEverything.getAverageRating();
+console.log(historyOfEverything.getAverageRating());
 
-const event2 = getRandEvent();
-const days2 = getTrainingDays(event2);
-const name2 = 'Warren';
-
-logEvent(event2, name2);
-logTime(days2, name2);
+let speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+speed.getAverageRating();
+console.log(speed.getAverageRating());
